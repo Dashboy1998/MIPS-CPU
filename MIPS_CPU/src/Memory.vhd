d2b5dc5f111626d2381c7f52170f6063f91e7404
Dashboy1998 <dashboy1998@gmail.com>
2019-02-18 00:09:42 -0500

    Removed bidirectional bus on Memory

--- MIPS_CPU/src/Memory.vhd
+++ MIPS_CPU/src/Memory.vhd
@@ -5,16 +5,14 @@ use IEEE.numeric_std.all;
 entity Memory is
   port(CS, WE, Clk: in std_logic;
        ADDR: in unsigned(31 downto 0);
-       Mem_Bus: inout unsigned(31 downto 0));
+       Mem_Bus: in unsigned(31 downto 0);
+	   output: out unsigned(31 downto 0));
 end Memory;
 
 architecture Internal of Memory is
   type RAMtype is array (0 to 127) of unsigned(31 downto 0);
   signal RAM1: RAMtype := (others => (others => '0'));
-  signal output: unsigned(31 downto 0);
 begin
-  Mem_Bus <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" when CS = '0' or WE = '1'
-    else output;
   process(Clk)
   begin
     if Clk = '0' and Clk'event then
